b8722deea9c1a23b22842578c579a08949a56d3f
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-03-18 04:03:43 +0800

    feat(app): add user api

---
+++ expo/user.js
@@ -0,0 +1,33 @@
+import {getStorage, setStorage} from "./storage";
+import db from './database';
+import {router} from "expo-router";
+
+async function isLogin() {
+    const session = db.auth.getUser();
+}
+
+async function register(email, password) {
+    console.log("register")
+    const {data, error} = await db.auth.signUp({email, password})
+    console.log("1")
+
+    if (error) {
+        throw error;
+    } else {
+        console.log(data);
+    }
+}
+
+async function login(email, password) {
+    const {data, error} = await db.auth.signInWithPassword({email, password})
+    if (error) throw error;
+    if (!error && !data) throw new Error("Please confirm registration via email");
+    return data;
+}
+
+async function logout() {
+    await db.auth.signOut();
+    router.navigate("/");
+}
+
+export default {isLogin, register, login, logout};
