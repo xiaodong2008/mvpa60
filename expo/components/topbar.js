ac9f930199c067354be476163fe2f714cbe539ce
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-03-18 04:03:25 +0800

    feat(app): update component topbar

--- expo/components/topbar.js
+++ expo/components/topbar.js
@@ -13,7 +13,11 @@ export default function Topbar(props) {
         <View style={styles.container}>
             {props.history && <FontAwesome5 name="history" size={20} color="#41C9E2" style={styles.left} />}
             <Text style={styles.title}>{props.title}</Text>
-            {props.post && <Feather name="plus-circle" size={22} color="#41C9E2" style={styles.right} />}
+            {props.post && (
+                    <Pressable style={styles.right} onPress={props.post}>
+                        <Feather name="plus-circle" size={22} color="#41C9E2"/>
+                    </Pressable>
+                )}
         </View>
     )
 }
@@ -23,25 +27,27 @@ const styles = StyleSheet.create({
         height: 30,
         backgroundColor: 'white',
         borderBottomWidth: 1,
-        borderBottomColor: '#e5e5e5'
+        borderBottomColor: '#e5e5e5',
+        flexDirection: 'row',
+        alignItems: 'center',
+        position: 'relative'
     },
     title: {
         fontSize: 16,
         fontWeight: '600',
         textAlign: 'center',
+        width: '100%'
     },
     left: {
         position: 'absolute',
         left: 15,
         top: 0,
         bottom: 0,
-        justifyContent: 'center'
     },
     right: {
         position: 'absolute',
         right: 15,
         top: 0,
         bottom: 0,
-        justifyContent: 'center'
     }
 })
