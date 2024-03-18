1da566b93554cd558f3864d26ec5e908957b0979
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-03-19 07:29:17 +0800

    feat(app): update theme

--- expo/theme.js
+++ expo/theme.js
@@ -1,8 +1,17 @@
 export default {
     color: {
         background: 'rgba(187,187,187,0.24)',
+        gray: 'rgba(204,204,204,0.89)',
+        whiteGray: 'rgba(192,192,192,0.13)',
         main: '#fff',
         text: 'black',
         primary: '#41C9E2',
     },
+    styles: {
+        pageRoot: {
+            flex: 1,
+            height: '100%',
+            backgroundColor: 'rgba(187,187,187,0.24)'
+        }
+    }
 }
