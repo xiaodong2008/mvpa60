8ee74c44a88a4d8cd02c3e31a852b5800583b540
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-03-19 02:05:37 +0800

    feat(app): update router root

--- expo/app/_layout.js
+++ expo/app/_layout.js
@@ -3,29 +3,59 @@ import React, {useEffect} from 'react';
 import db from '../database';
 import {SafeAreaView, StyleSheet} from "react-native";
 
+import FlashMessage from "react-native-flash-message";
+import message from "../message";
+
 export const SessionContext = React.createContext();
 
 export default function HomeLayout() {
     const [session, setSession] = React.useState(undefined)
+    const [profile, setProfile] = React.useState(undefined)
 
     useEffect(() => {
-        const sub = db.auth.onAuthStateChange((event, session) => {
-            setSession(session)
-            console.log("set", session)
+        db.auth.getSession().then(({data: {session}}) => {
+            if (session) {
+                setSession(session)
+            }
         })
 
-        return () => {
-            sub.data.subscription.unsubscribe()
-        }
+        db.auth.onAuthStateChange((event, session) => {
+            if (event === 'SIGNED_OUT') setSession(null), setProfile(null)
+            else setSession(session)
+        })
     }, []);
 
+    useEffect(() => {
+        if (session) getProfile().then(r => setProfile(r))
+    }, [session])
+
     return (
-        <SessionContext.Provider value={session}>
+        <SessionContext.Provider
+            value={{
+                user: session?.user,
+                profile: profile,
+                updateProfile: () => getProfile().then(r => setProfile(r))
+            }}>
+            <FlashMessage position="top"/>
             <SafeAreaView style={styles.container}>
-                <Slot/>
+                <Slot name="main"/>
             </SafeAreaView>
         </SessionContext.Provider>
     );
+
+    async function getProfile() {
+        if (!session?.user) return;
+        const {data, error} = await db.from('userdata').select('*').eq('user_id', session.user.id).single()
+        if (error) return message.error(error.message)
+
+        // Setup user data
+        if (!data) {
+            const {data, error} = await db.from("userdata").insert({nickname: email.split("@")[0], data: {}})
+            if (error) return message.error(error.message)
+            return data;
+        }
+        return data;
+    }
 }
 
 const styles = StyleSheet.create({
