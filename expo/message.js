fcd41da480c204f9e30d6f8cb591e6e378d4e05f
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-03-19 07:36:05 +0800

    feat(app): add message api

---
+++ expo/message.js
@@ -0,0 +1,34 @@
+import {showMessage} from "react-native-flash-message";
+
+function success(msg) {
+    showMessage({
+        message: msg,
+        type: "success",
+        duration: 5000,
+        icon: "success"
+    });
+}
+
+function warning(msg) {
+    showMessage({
+        message: msg,
+        type: "warning",
+        duration: 5000,
+        icon: "warning"
+    });
+}
+
+function error(msg) {
+    showMessage({
+        message: msg,
+        type: "danger",
+        duration: 5000,
+        icon: "danger"
+    });
+}
+
+export default {
+    success,
+    warning,
+    error
+};
