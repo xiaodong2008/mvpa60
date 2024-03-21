6f11b51fe6a5455dc7a9241a8183e8bf841c0be4
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-03-22 01:16:14 +0800

    Refactor home page to use useContext hook in main.js

--- expo/app/home/main.js
+++ expo/app/home/main.js
@@ -1,27 +1,36 @@
 import Card from "../../components/card";
-import {Feather, FontAwesome5} from "@expo/vector-icons";
-import {ScrollView, View} from "react-native";
+import { Feather, FontAwesome5 } from "@expo/vector-icons";
+import { ScrollView, View } from "react-native";
+import { useContext } from "react";
+import { SessionContext } from "../_layout";
+
 import Topbar from "../../components/topbar";
 
-export default function Home({navigation}) {
+export default function Home({ navigation }) {
+    const session = useContext(SessionContext)
+
     return (
         <View style={styles.container}>
             <Topbar title="Home"
-                    history={true}
-                    post={() => navigation.navigate('Post')}
-                    leftIcon={<FontAwesome5 name="history" size={20} color="#41C9E2" style={styles.left} />}
-                    rightIcon={<Feather name="plus-circle" size={22} color="#41C9E2"/>}
-                    rightPress={() => navigation.navigate('Post')}
+                history={true}
+                post={() => navigation.navigate('Post')}
+                leftIcon={<FontAwesome5 name="history" size={20} color="#41C9E2" style={styles.left} />}
+                rightIcon={<Feather name="plus-circle" size={22} color="#41C9E2" />}
+                rightPress={() => navigation.navigate('Post')}
             />
-            <ScrollView>
-                <Card icon={<FontAwesome5 name="running" size={24} color="black"/>}
-                      title="Do a quick run" btn="Start exercise >"
-                      text="Running is a great way to get some exercise and you can do it anywhere, anytime."/>
-                <Card icon={<FontAwesome5 name="user-friends" size={20} color="black"/>}
-                      title="Write a post" btn="Write now >"
-                      onPress={() => navigation.navigate('Post')}
-                      text="Just broke through your personal best, or maybe you just want to share your thoughts? Write a post and share it with MVPA community."/>
-            </ScrollView>
+            {
+                session.user && (
+                    <ScrollView>
+                        <Card icon={<FontAwesome5 name="running" size={24} color="black" />}
+                            title="Do a quick run" btn="Start exercise >"
+                            text="Running is a great way to get some exercise and you can do it anywhere, anytime." />
+                        <Card icon={<FontAwesome5 name="user-friends" size={20} color="black" />}
+                            title="Write a post" btn="Write now >"
+                            onPress={() => navigation.navigate('Post')}
+                            text="Just broke through your personal best, or maybe you just want to share your thoughts? Write a post and share it with MVPA community." />
+                    </ScrollView>
+                )
+            }
         </View>
     )
 }
