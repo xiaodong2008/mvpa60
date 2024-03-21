910f683125da96c1fc65da1db52972d6fb7f4c71
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-03-22 01:16:06 +0800

    Refactor home page to use createNativeStackNavigator in index.js

--- expo/app/home/index.js
+++ expo/app/home/index.js
@@ -1,16 +1,15 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useContext, useEffect } from "react";
+import { SessionContext } from "../_layout";
+import { View } from "react-native";
+import theme from "../../theme";
+
 import Main from "./main";
 import Post from "./post";
 import Banner from "../../components/banner";
-
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {useContext, useEffect} from "react";
-import {SessionContext} from "../_layout";
-import user from "../../user";
-import {View} from "react-native";
-import theme from "../../theme";
 const Stack = createNativeStackNavigator();
 
-export default function Page({navigation}) {
+export default function Page({ navigation }) {
     const session = useContext(SessionContext)
 
     useEffect(() => {
@@ -20,15 +19,11 @@ export default function Page({navigation}) {
 
     return (
         <View style={theme.styles.pageRoot}>
-            {
-                session.user && (
-                    <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
-                        <Stack.Screen name="Home" component={Main}/>
-                        <Stack.Screen name="Post" component={Post}/>
-                    </Stack.Navigator>
-                )
-            }
-            <Banner active="home"/>
+            <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Home" component={Main} />
+                <Stack.Screen name="Post" component={Post} />
+            </Stack.Navigator>
+            <Banner active="home" />
         </View>
     )
 }
