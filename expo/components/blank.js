e1960c1537b8ab8b04da4e3a5299ad7a67e23c0e
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-04-09 15:47:38 +0800

    Add blank component to expo/components/blank.js

---
+++ expo/components/blank.js
@@ -0,0 +1,10 @@
+import {View} from "react-native"
+
+export default function Blank(props) {
+  return (
+    <View style={{
+      height: props.height || 0,
+      width: props.width || 0 
+    }}></View>
+  )
+}
