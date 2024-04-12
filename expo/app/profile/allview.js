40b34b95eafd25837b9ce143a4f552b3aa0115ce
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-04-12 21:47:15 +0800

    feat(profile): add AllView component

---
+++ expo/app/profile/allview.js
@@ -0,0 +1,12 @@
+import {View, Text, StyleSheet} from "react-native";
+
+export default function AllView() {
+    return (
+        <View>
+            <Text>AllView</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+})
