588d958701dbec2d1b5b709caf88ac2addb60bb8
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-04-14 09:54:55 +0800

    refactor(app): refactor _layout.js

--- expo/app/_layout.js
+++ expo/app/_layout.js
@@ -1,7 +1,7 @@
-import {Slot} from 'expo-router';
-import React, {useEffect} from 'react';
-import db from '../database';
-import {View, StyleSheet, Image} from "react-native";
+import { Slot } from "expo-router";
+import React, { useEffect } from "react";
+import db from "../database";
+import { View, StyleSheet, Image } from "react-native";
 
 import FlashMessage from "react-native-flash-message";
 import message from "../message";
@@ -9,26 +9,28 @@ import message from "../message";
 export const SessionContext = React.createContext();
 
 export default function HomeLayout() {
-    const [session, setSession] = React.useState(undefined)
-    const [profile, setProfile] = React.useState(undefined)
+    const [session, setSession] = React.useState(undefined);
+    const [profile, setProfile] = React.useState(undefined);
 
     useEffect(() => {
-        db.auth.getSession().then(({data: {session}}) => {
-            if (session) {
-                setSession(session)
-            }
-        })
+        db.auth.getSession().then(({ data: { session } }) => {
+            if (session) setSession(session);
+        });
 
         db.auth.onAuthStateChange((event, session) => {
-            if (event === 'SIGNED_OUT') setSession(null), setProfile(null)
-            else setSession(session)
-            console.log(event, session)
-        })
+            if (event === "SIGNED_OUT") {
+                setSession(null);
+                setProfile(null);
+            } else setSession(session);
+            console.log(
+                `[_layout.js] User ${session ? "signed in" : "signed out"}`
+            );
+        });
     }, []);
 
     useEffect(() => {
-        if (session) getProfile().then(r => setProfile(r))
-    }, [session])
+        if (session) getProfile().then((r) => setProfile(r));
+    }, [session]);
 
     return (
         <SessionContext.Provider
@@ -38,26 +40,36 @@ export default function HomeLayout() {
                     ...profile?.public_data,
                     ...profile?.private_data,
                 },
-                updateProfile: () => getProfile().then(r => setProfile(r)),
+                updateProfile: () => getProfile().then((r) => setProfile(r)),
                 clearSession: () => setSession(null),
-            }}>
-            <FlashMessage position="top"/>
-            <Image style={styles.background} source={require('../assets/background.png')}/>
+            }}
+        >
+            <FlashMessage position="top" />
+            <Image
+                style={styles.background}
+                source={require("../assets/background.png")}
+            />
             <View style={styles.container}>
-                <Slot name="main"/>
+                <Slot name="main" />
             </View>
         </SessionContext.Provider>
     );
 
     async function getProfile() {
         if (!session?.user) return;
-        const {data, error} = await db.from('userdata').select('*').eq('user_id', session.user.id).single()
-        if (error) return message.error(error.message)
+        const { data, error } = await db
+            .from("userdata")
+            .select("*")
+            .eq("user_id", session.user.id)
+            .single();
+        if (error) return message.error(error.message);
 
         // Setup user data
         if (!data) {
-            const {data, error} = await db.from("userdata").insert({nickname: email.split("@")[0], data: {}})
-            if (error) return message.error(error.message)
+            const { data, error } = await db
+                .from("userdata")
+                .insert({ nickname: email.split("@")[0], data: {} });
+            if (error) return message.error(error.message);
             return data;
         }
         return data;
@@ -66,13 +78,13 @@ export default function HomeLayout() {
 
 const styles = StyleSheet.create({
     container: {
-        height: '100%',
-        flex: 1
+        height: "100%",
+        flex: 1,
     },
     background: {
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        zIndex: 0
-    }
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        zIndex: 0,
+    },
 });
